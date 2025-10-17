17d44a8d1466879711b41f19e0282a4b12fcc7a4
Kaustubh Sen <kaustubhsen2707@gmail.com>
2025-10-18 01:07:44 +0530

    linear search algorithm (array)

---
+++ linearsearch algo array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int linearsearch(int arr[], int sz, int target) {
+    for (int i = 0; i<sz; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {5,2,7,9,8,10,1};
+    int sz = 7;
+    int target = 8;
+    cout<<linearsearch(arr, sz, target);
+    return 0;
+}
